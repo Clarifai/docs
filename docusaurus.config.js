4768ffaa5760149892010117e5edab21af348563
Ian Kelk <iankelk@gmail.com>
2022-01-31 12:57:54 -0500

    Added Google tracking ID

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,6 +31,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'UA-115983722-1',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
