9cd99f709671c3074b39909cb7f20493c640a1c5
Ian Kelk <iankelk@gmail.com>
2022-01-31 10:59:50 -0500

    Change baseUrl to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Clarifai Guide',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/docs-new/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
