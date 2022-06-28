e4eff8c116b03dfd6d88243c771a8ca47bcf1da0
Alfrick <alfo.opidi85@gmail.com>
2022-06-28 20:22:44 +0300

    Corrected extra spaces in Java files

--- code_snippets/api-guide/data/create_get_update_delete/java/list_inputs_streaming.java
+++ code_snippets/api-guide/data/create_get_update_delete/java/list_inputs_streaming.java
@@ -8,62 +8,62 @@ import java.util.List;
 
 public class ClarifaiExample {
 
-    ////////////////////////////////////////////////////////////////////
-    // In this section, we set the user authentication and app ID. 
+    ////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication and app ID. 
     // Change these strings to run your own example.
-    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
 
-    static final String USER_ID = "YOUR_USER_ID_HERE";
-    //Your PAT (Personal Access Token) can be found in the portal under Authentication
-    static final String PAT = "YOUR_PAT_HERE";
-    static final String APP_ID = "YOUR_APP_ID_HERE";
-       
-    ///////////////////////////////////////////////////////////////////////////////////
-    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-    ///////////////////////////////////////////////////////////////////////////////////	
+    static final String USER_ID = "YOUR_USER_ID_HERE";
+    //Your PAT (Personal Access Token) can be found in the portal under Authentication
+    static final String PAT = "YOUR_PAT_HERE";
+    static final String APP_ID = "YOUR_APP_ID_HERE";
 
-    public static void main(String[] args) {
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////	
 
-        V2Grpc.V2BlockingStub stub = V2Grpc.newBlockingStub(ClarifaiChannel.INSTANCE.getGrpcChannel())
-            .withCallCredentials(new ClarifaiCallCredentials(PAT));             
-       
-        // To start from beginning, do not provide the last ID parameter.
-        MultiInputResponse firstStreamInputsResponse = stub.streamInputs(
-            StreamInputsRequest.newBuilder()
-            	.setUserAppId(UserAppIDSet.newBuilder().setUserId(USER_ID).setAppId(APP_ID))
-                .setPerPage(10)
-                .build()
-        );
+    public static void main(String[] args) {
 
-        if (firstStreamInputsResponse.getStatus().getCode() != StatusCode.SUCCESS) {
-            throw new RuntimeException("Stream inputs failed, status: " + firstStreamInputsResponse.getStatus());
-        }
+        V2Grpc.V2BlockingStub stub = V2Grpc.newBlockingStub(ClarifaiChannel.INSTANCE.getGrpcChannel())
+            .withCallCredentials(new ClarifaiCallCredentials(PAT));
 
-        System.out.println("First response (starting from the first input):");
-        List<Input> inputs = firstStreamInputsResponse.getInputsList();
-        for (Input input : inputs) {
-            System.out.println("\t" + input.getId());
-        }
+        // To start from beginning, do not provide the last ID parameter.
+        MultiInputResponse firstStreamInputsResponse = stub.streamInputs(
+            StreamInputsRequest.newBuilder()
+            .setUserAppId(UserAppIDSet.newBuilder().setUserId(USER_ID).setAppId(APP_ID))
+            .setPerPage(10)
+            .build()
+        );
 
-        String lastId = inputs.get(inputs.size() - 1).getId();
+        if (firstStreamInputsResponse.getStatus().getCode() != StatusCode.SUCCESS) {
+            throw new RuntimeException("Stream inputs failed, status: " + firstStreamInputsResponse.getStatus());
+        }
 
-        // Set last ID to get the next set of inputs. The returned inputs will not include the last ID input.
-        MultiInputResponse secondStreamInputsResponse = stub.streamInputs(
-            StreamInputsRequest.newBuilder()
-            	.setUserAppId(UserAppIDSet.newBuilder().setUserId(USER_ID).setAppId(APP_ID))
-                .setLastId(lastId)
-                .setPerPage(10)
-                .build()
-        );
+        System.out.println("First response (starting from the first input):");
+        List < Input > inputs = firstStreamInputsResponse.getInputsList();
+        for (Input input: inputs) {
+            System.out.println("\t" + input.getId());
+        }
 
-        if (secondStreamInputsResponse.getStatus().getCode() != StatusCode.SUCCESS) {
-            throw new RuntimeException("Stream inputs failed, status: " + secondStreamInputsResponse.getStatus());
-        }
+        String lastId = inputs.get(inputs.size() - 1).getId();
 
-        System.out.println(String.format("Second response (first input is the one following input ID %s)", lastId));
-        for (Input input : secondStreamInputsResponse.getInputsList()) {
-            System.out.println("\t" + input.getId());
-        }          
-        	
-    }  
+        // Set last ID to get the next set of inputs. The returned inputs will not include the last ID input.
+        MultiInputResponse secondStreamInputsResponse = stub.streamInputs(
+            StreamInputsRequest.newBuilder()
+            .setUserAppId(UserAppIDSet.newBuilder().setUserId(USER_ID).setAppId(APP_ID))
+            .setLastId(lastId)
+            .setPerPage(10)
+            .build()
+        );
+
+        if (secondStreamInputsResponse.getStatus().getCode() != StatusCode.SUCCESS) {
+            throw new RuntimeException("Stream inputs failed, status: " + secondStreamInputsResponse.getStatus());
+        }
+
+        System.out.println(String.format("Second response (first input is the one following input ID %s)", lastId));
+        for (Input input: secondStreamInputsResponse.getInputsList()) {
+            System.out.println("\t" + input.getId());
+        }
+
+    }
 }
--- code_snippets/api-guide/predict/java/images_via_bytes.java
+++ code_snippets/api-guide/predict/java/images_via_bytes.java
@@ -10,61 +10,61 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class ClarifaiExample {
-	
-	////////////////////////////////////////////////////////////////////////////////////
-	// In this section, we set the user authentication, app ID, and the location
-	// of the image we want as an input. Change these strings to run your own example.
-	/////////////////////////////////////////////////////////////////////////////////////
-	
-	static final String USER_ID = "YOUR_USER_ID_HERE";
-	// Your PAT (Personal Access Token) can be found in the portal under Authentication
-	static final String PAT = "YOUR_PAT_HERE";
-	static final String APP_ID = "YOUR_APP_ID_HERE";
-	static final String MODEL_ID = "YOUR_MODEL_ID_HERE";
-	// Change this to whatever image location you want to process
-	static final String IMAGE_FILE_LOCATION = "YOUR_IMAGE_FILE_LOCATION";
-	// This is optional.You can specify a model version or an empty string for the default
-	static final String MODEL_VERSION_ID = "";		
-	
-	///////////////////////////////////////////////////////////////////////////////////
-	// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-	///////////////////////////////////////////////////////////////////////////////////	
 
-	public static void main(String[] args) throws IOException {
-		
-		V2Grpc.V2BlockingStub stub = V2Grpc.newBlockingStub(ClarifaiChannel.INSTANCE.getGrpcChannel())
-			    .withCallCredentials(new ClarifaiCallCredentials(PAT));		
+    ////////////////////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication, app ID, and the location
+    // of the image we want as an input. Change these strings to run your own example.
+    /////////////////////////////////////////////////////////////////////////////////////
 
-		MultiOutputResponse postModelOutputsResponse = stub.postModelOutputs(
-		    PostModelOutputsRequest.newBuilder()
-		        .setUserAppId(UserAppIDSet.newBuilder().setUserId(USER_ID).setAppId(APP_ID))
-		        .setModelId(MODEL_ID)
-		        .setVersionId(MODEL_VERSION_ID)  // This is optional. Defaults to the latest model version
-		        .addInputs(
-		            Input.newBuilder().setData(
-		                Data.newBuilder().setImage(
-		                    Image.newBuilder()
-		                        .setBase64(ByteString.copyFrom(Files.readAllBytes(
-		                            new File(IMAGE_FILE_LOCATION).toPath()
-		                        )))
-		                )
-		            )
-		        )
-		        .build()
-		);
+    static final String USER_ID = "YOUR_USER_ID_HERE";
+    //Your PAT (Personal Access Token) can be found in the portal under Authentication	
+    static final String PAT = "YOUR_PAT_HERE";
+    static final String APP_ID = "YOUR_APP_ID_HERE";
+    static final String MODEL_ID = "YOUR_MODEL_ID_HERE";
+    // Change this to whatever image location you want to process
+    static final String IMAGE_FILE_LOCATION = "YOUR_IMAGE_FILE_LOCATION";
+    // This is optional.You can specify a model version or an empty string for the default
+    static final String MODEL_VERSION_ID = "";
 
-		if (postModelOutputsResponse.getStatus().getCode() != StatusCode.SUCCESS) {
-		  throw new RuntimeException("Post model outputs failed, status: " + postModelOutputsResponse.getStatus());
-		}
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////	
 
-		// Since we have one input, one output will exist here.
-		Output output = postModelOutputsResponse.getOutputs(0);
+    public static void main(String[] args) throws IOException {
 
-		System.out.println("Predicted concepts:");
-		for (Concept concept : output.getData().getConceptsList()) {
-		    System.out.printf("%s %.2f%n", concept.getName(), concept.getValue());
-		}
+        V2Grpc.V2BlockingStub stub = V2Grpc.newBlockingStub(ClarifaiChannel.INSTANCE.getGrpcChannel())
+            .withCallCredentials(new ClarifaiCallCredentials(PAT));
 
-	}
+        MultiOutputResponse postModelOutputsResponse = stub.postModelOutputs(
+            PostModelOutputsRequest.newBuilder()
+            .setUserAppId(UserAppIDSet.newBuilder().setUserId(USER_ID).setAppId(APP_ID))
+            .setModelId(MODEL_ID)
+            .setVersionId(MODEL_VERSION_ID) // This is optional. Defaults to the latest model version
+            .addInputs(
+                Input.newBuilder().setData(
+                    Data.newBuilder().setImage(
+                        Image.newBuilder()
+                        .setBase64(ByteString.copyFrom(Files.readAllBytes(
+                            new File(IMAGE_FILE_LOCATION).toPath()
+                        )))
+                    )
+                )
+            )
+            .build()
+        );
+
+        if (postModelOutputsResponse.getStatus().getCode() != StatusCode.SUCCESS) {
+            throw new RuntimeException("Post model outputs failed, status: " + postModelOutputsResponse.getStatus());
+        }
+
+        // Since we have one input, one output will exist here.
+        Output output = postModelOutputsResponse.getOutputs(0);
+
+        System.out.println("Predicted concepts:");
+        for (Concept concept: output.getData().getConceptsList()) {
+            System.out.printf("%s %.2f%n", concept.getName(), concept.getValue());
+        }
+
+    }
 
 }
