6fa1fa67bf2d0c5f00cde38e38bd26761fc7c174
Alfrick <alfo.opidi85@gmail.com>
2022-10-05 18:43:58 +0300

    Added evaluate curl samples

---
+++ code_snippets/api-guide/evaluate/interpret_evaluations.sh
@@ -0,0 +1,4 @@
+curl -X GET "https://api.clarifai.com/v2/users/YOUR_USER_ID_HERE/apps/YOUR_APP_ID_HERE/models/YOUR_MODEL_ID_HERE/versions/YOUR_MODEL_VERSION_ID_HERE/metrics" \
+  -H "Authorization: Key YOUR_PAT_HERE" \
+  -H "Content-Type: application/json" 
+  
