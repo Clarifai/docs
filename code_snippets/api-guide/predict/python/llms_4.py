532d280af238cbd28fec4b314fc86ddc0baaf5aa
Alfrick <alfo.opidi85@gmail.com>
2023-10-09 19:44:15 +0300

    published annotation writer page

--- code_snippets/api-guide/predict/python/llms_4.py
+++ code_snippets/api-guide/predict/python/llms_4.py
@@ -1,5 +1,4 @@
 # Use our new Python SDK to make predictions quickly using a few lines of code
-# Learn how to install it here: https://clarifai-python.readthedocs.io/en/latest/install.html
 
 from clarifai.client.model import Model
 text_url = "https://samples.clarifai.com/negative_sentence_12.txt"
