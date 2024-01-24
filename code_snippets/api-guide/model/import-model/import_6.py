dd3c3d2f474909ac5728379d29fb11e78c299227
Alfrick <alfo.opidi85@gmail.com>
2024-01-24 19:44:09 +0300

    removed clarifai help sidebar link

---
+++ code_snippets/api-guide/model/import-model/import_6.py
@@ -0,0 +1,52 @@
+########################################################################################
+# In this section, we set the user authentication, app ID, and model ID.
+# Change these strings to run your own example.
+########################################################################################
+
+USER_ID = "YOUR_USER_ID_HERE"
+# Your PAT (Personal Access Token) can be found in the portal under Authentification
+PAT = "YOUR_PAT_HERE"
+APP_ID = "YOUR_APP_ID_HERE"
+# Change these to import your own model
+MODEL_ID = "embedding-model"
+MODEL_ZIP_URL = 's3://clarifai-testing-persistent-data/test_triton_upload/jina-embeddings-v2-base-en.zip'
+
+##########################################################################
+# YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+##########################################################################
+
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
+channel = ClarifaiChannel.get_grpc_channel()
+stub = service_pb2_grpc.V2Stub(channel)
+
+metadata = (("authorization", "Key " + PAT),)
+
+userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+
+post_model_versions = stub.PostModelVersions(
+    service_pb2.PostModelVersionsRequest(
+        user_app_id=userDataObject,
+        model_id=MODEL_ID,
+        model_versions=[
+            resources_pb2.ModelVersion(
+                pretrained_model_config=resources_pb2.PretrainedModelConfig(
+                    model_zip_url=MODEL_ZIP_URL,
+                    input_fields_map=resources_pb2.InputFieldsMap(
+                        text='text'
+                    )                
+                ),
+                output_fields_map=resources_pb2.OutputFieldsMap(
+                    embeddings='embeddings'
+                )  
+            )
+        ],
+    ),
+    metadata=metadata,
+)
+
+if post_model_versions.status.code != status_code_pb2.SUCCESS:
+    print(post_model_versions.status)
+    raise Exception("Post models versions failed, status: " + post_model_versions.status.description)
---
+++ code_snippets/api-guide/model/import-model/import_6.sh
@@ -0,0 +1,17 @@
+curl -X POST "https://api.clarifai.com/v2/users/YOUR_USER_ID_HERE/apps/YOUR_APP_ID_HERE/models/embedding-model/versions" \
+  -H "Authorization: Key YOUR_PAT_HERE" \
+  -H "Content-Type: application/json" \
+  -d '{
+       "model_versions": [{
+            "pretrained_model_config": {
+                "model_zip_url": "s3://clarifai-testing-persistent-data/test_triton_upload/jina-embeddings-v2-base-en.zip",
+                "input_fields_map": {
+                    "text": "text"
+            },
+            "output_fields_map": {
+                "embeddings": "embeddings"
+                }
+            }
+            
+        }]
+     }'
