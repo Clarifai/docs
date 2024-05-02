bd4b08c76d850256f1accc8a5261693b1d982872
adithyan sukumar <adithyan.sukumar@clarifai.com>
2024-05-02 14:27:40 +0530

    fixed code snipppets

--- code_snippets/python-sdk/workflows/text_classifier.py
+++ code_snippets/python-sdk/workflows/text_classifier.py
@@ -7,5 +7,6 @@ workflow_url = "https://clarifai.com/clarifai/main/workflows/Text-Moderation"
 text_classfication_workflow = Workflow(
     url= workflow_url , pat="YOUR_PAT"
 )
-result = text_classfication_workflow.predict_by_bytes(b"I hate you", input_type="text")
+raw_text = b"I love your product very much"
+result = text_classfication_workflow.predict_by_bytes(raw_text, input_type="text")
 print(result.results[0].outputs[0].data)
