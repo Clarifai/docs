ece0da855d9969b5d90a9769259344d3e43ef388
DaniAkash <dani@clarifai.com>
2024-06-11 14:12:44 +0530

    feat: setup redocly client only page

---
+++ src/components/redocly.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+
+export default function Redocly() {
+    useEffect(() => {
+        const checkRedoc = (callback, retries = 10, interval = 100) => {
+            if (typeof Redoc !== 'undefined') {
+                callback();
+            } else if (retries > 0) {
+                setTimeout(() => checkRedoc(callback, retries - 1, interval), interval);
+            } else {
+                console.error('Redoc is not available after multiple retries.');
+            }
+        };
+
+        const initRedoc = () => {
+            Redoc.init(window.location.origin + '/api-spec/clarifai.json', {
+                theme: { colors: { primary: { main: '#0c38f6' } } },
+            }, document.querySelector('#redocly-ui'));
+        };
+
+        checkRedoc(initRedoc);
+    }, []);
+
+    return (
+        <div id="redocly-ui"/>
+    );
+}
