4e9657fef4cf3204c27c80f6c9df6be400dcc13a
Alfrick <alfo.opidi85@gmail.com>
2024-06-21 20:18:05 +0300

    updated custom templates

--- code_snippets/api-guide/others/create-your-own-template.py
+++ code_snippets/api-guide/others/create-your-own-template.py
@@ -1,80 +1,145 @@
 # Base Configuration File
 # This configuration file extends an existing YOLOF model configuration.
 
-_base_ = '/mmdetection/configs/yolof/yolof_r50_c5_8x8_1x_coco.py'
+_base_ = '/mmdetection/configs/yolof/yolof_r50-c5_8xb8-1x_coco.py'
 
-# Model Configuration
+# Load from a checkpoint
+load_from = 's3://clarifai-data/training/checkpoint/mmdetection/yolof_r50_c5_8x8_1x_coco_20210425_024427-8e864411.pth'
+
+# Minimum number of samples per epoch
+min_samples_per_epoch = 30
+
+# Dataset configuration
+dataset_type = 'CocoDataset'
+
+# Model configuration
 model = dict(
-    type='YOLOF',  # Specify the YOLOF model
-    pretrained='torchvision://resnet50',  # Pretrained model (if available)
+    type='YOLOF',  # Specifies that the model type is YOLOF
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',  # Specifies the type of data preprocessor
+        mean=[123.675, 116.28, 103.53],  # Mean values used to pre-train the backbone models, ordered in R, G, B
+        std=[1.0, 1.0, 1.0],  # Standard variance used to pre-train the backbone models, ordered in R, G, B
+        bgr_to_rgb=True,  # Whether to convert image from BGR to RGB
+        pad_mask=True,  # Whether to pad instance masks
+        pad_size_divisor=32,  # The size of the padded image should be divisible by pad_size_divisor, ensuring compatibility with the network's downsampling operations
+    ),
     backbone=dict(
-        type='ResNet',  # Specify the backbone network (e.g., 'ResNet')
-        depth=50  # Specify the depth of the backbone (e.g., ResNet-50)
+        type='ResNet',  # Specifies the backbone network (e.g., 'ResNet')
+        depth=50,  # Specify the depth of the backbone (e.g., ResNet-50)
+        frozen_stages=1,  # The first stage of the backbone will not be updated during training, helping to preserve low-level features
+        init_cfg=dict(),  # Indicates how the weights of the backbone network are initialized
     ),
     neck=dict(
-        type='YOLOFNeck',  # Specify the neck architecture (e.g., 'YOLOFNeck')
-        in_channels=[256, 512, 1024, 2048],  # Input channels from the backbone
-        out_channels=256,  # Output channels for the neck
-        num_csp_blocks=4  # Number of CSP blocks in the neck
+        type='DilatedEncoder',  # Specifies the neck type, which is part of the YOLOF architecture
+        in_channels=[256, 512, 1024, 2048],  # Input channels from different stages of the backbone
+        out_channels=256,  # Number of output channels after processing through the neck
     ),
     bbox_head=dict(
-        type='YOLOFHead',
-        num_classes=80,  # Number of object classes in your dataset. It must be included with any value, and it will be updated based on your dataset's number of classes
-        in_channels=256,  # Number of input channels from the neck
-        num_levels=5,  # Number of levels used in the detection head
-        reg_decoded_bbox=True,  # Whether to decode bounding box regression targets
-        loss_bbox=dict(type='CIoULoss', loss_weight=1.0),  # Specify the bounding box loss type
-        loss_conf=dict(type='CIoULoss', loss_weight=1.0)  # Specify the confidence loss type
-        roi_feat_size=7,  # RoI feature size
-        roi_out_channels=256,  # RoI feature output channels
-    )
-)
-
-# Data Configuration
-# This section should include 'train' and 'val' sections, each with 'ann_file', 'img_prefix', and 'classes' fields with empty strings as values
-# These values will be overwritten to be compatible with Clarifai's system, but must be included in the imported config
-data = dict(
-    # Data Loader Configuration
-    samples_per_gpu=4,  # Number of samples processed on each GPU
-    workers_per_gpu=4,  # Number of data-loading workers per GPU
-    train=dict(
-        dataset=dict(            
-            ann_file='',  # Path to training dataset annotations
-            img_prefix='',  # Directory containing training images
-            classes=''  # List of class names in your dataset
-        ), 
+        type='YOLOFHead',  # Specifies the head type for YOLOF, responsible for generating bounding boxes and class predictions
+        num_classes=80,  # Number of object classes for classification
+        in_channels=256,  # Number of input channels for bbox head
+        out_channels=256,  # Number of output channels of the extracted feature
     ),
-    val=dict(
-        dataset=dict(            
-            ann_file='',  # Path to validation dataset annotations
-            img_prefix='',  # Directory containing validation images
-            classes=''  # List of class names in your dataset
-        ),
+)
+
+# Optimizer configuration
+optim_wrapper = dict(
+    type='OptimWrapper',  # Type of optimizer wrapper, you can switch to AmpOptimWrapper to enable mixed precision training
+    optimizer=dict(  # Optimizer configuration, supports various PyTorch optimizers, please refer to https://pytorch.org/docs/stable/optim.html#algorithms
+        type='SGD',  # SGD
+        lr=0.001875,  # Base learning rate
+        momentum=0.9,  # SGD with momentum
+        weight_decay=0.0001,
+    ),  # Weight decay
+    paramwise_cfg=dict(
+        norm_decay_mult=0.0, custom_keys=dict(backbone=dict(lr_mult=0.3333))
     ),
-    
+    clip_grad=dict(
+        max_norm=8, norm_type=2
+    ),  # Configuration for gradient clipping, set to None to disable. For usage, please see https://mmengine.readthedocs.io/en/latest/tutorials/optimizer.html
 )
 
-# Optimizer Configuration
-optimizer = dict(
-    _delete_=True,  # Delete existing optimizer settings
-    type='Adam',  # Use the Adam optimizer
-    lr=0.0001,  # Set the learning rate
-    weight_decay=0.0001  # Weight decay for regularization
+# Parameter scheduler configuration
+param_scheduler = [
+    dict(
+        type='LinearLR',  # Use linear learning rate warmup
+        start_factor=0.00066667,  # Coefficient for learning rate warmup
+        by_epoch=False,  # Update the learning rate during warmup at each iteration
+        begin=0,  # Start updating the parameters from the first iteration
+        end=500,
+    ),  # End the warmup at the 500th iteration
+    dict(
+        type='MultiStepLR',  # Use multi-step learning rate strategy during training
+        by_epoch=True,
+        begin=0,
+        end=12,  # Ending at the 12th epoch
+        milestones=[8, 12],  # Learning rate decay at which epochs
+        gamma=0.1,  # Learning rate decay coefficient
+    ),
+]
+
+# Hook configuration
+custom_hooks = [
+    dict(type='CheckInvalidLossHook', interval=50)
+]  # Regularly checks if the loss is valid during training; checks every 50 iterations
+
+# Dataset and evaluator configuration
+train_pipeline = [  # Training data processing pipeline
+    dict(type='LoadImageFromFile'),  # First pipeline to load images from file path
+    dict(
+        type='LoadAnnotations', with_bbox=True
+    ),  # Second pipeline to load annotations for current image
+    dict(
+        type='Resize', scale=(768, 512), keep_ratio=1.5
+    ),  # Pipeline that resizes the images and their annotations
+    dict(
+        type='RandomFlip', prob=0.5
+    ),  # Augmentation pipeline that flips the images and their annotations
+    dict(type='RandomShift', prob=0.5, max_shift_px=32),
+    dict(
+        type='PackDetInputs'
+    ),  # Pipeline that formats the annotation data and decides which keys in the data should be packed into data_samples
+]
+
+test_pipeline = None  # Testing data processing pipeline
+
+train_dataloader = dict(  # Train dataloader config
+    batch_size=16,  # Batch size of a single GPU
+    persistent_workers=True,  # If True, the dataloader will not shut down the worker processes after an epoch end, which can accelerate training speed
+    sampler=dict(
+        type='DefaultSampler', shuffle=True
+    ),  # Default sampler, supports both distributed and non-distributed training
+    batch_sampler=dict(
+        type='AspectRatioBatchSampler'
+    ),  # Default batch_sampler, used to ensure that images in the batch have similar aspect ratios, so as to better utilize graphics memory
+    dataset=dict(  # Train dataset config
+        type=dataset_type,
+        data_root='',
+        ann_file='',  # Path of annotation file
+        data_prefix=dict(img=''),  # Prefix of image path
+        metainfo=dict(classes=()),
+        filter_cfg=dict(
+            filter_empty_gt=True, min_size=32
+        ),  # Config of filtering images and annotations
+        pipeline=train_pipeline,
+    ),
+)
+# In version 3.x, validation and test dataloaders can be configured independently
+val_dataloader = None  # Validation dataloader config
+
+val_evaluator = None  # Validation evaluator config
+
+# Training and testing configuration
+train_cfg = dict(
+    type='EpochBasedTrainLoop',  # Type of training loop, please refer to https://github.com/open-mmlab/mmengine/blob/main/mmengine/runner/loops.py
+    max_epochs=15,  # Maximum number of training epochs
+    val_interval=1,  # Validation intervals. Run validation every epoch
 )
 
-# Learning Rate Schedule Configuration
-lr_config = dict(
-    _delete_=True,  # Delete existing learning rate schedule settings
-    policy='CosineAnnealing',  # Learning rate schedule policy
-    warmup='linear',  # Warm-up strategy
-    warmup_iters=1000,  # Number of warm-up iterations
-    warmup_ratio=0.1,  # Ratio for warm-up learning rate
-    min_lr_ratio=1e-5  # Minimum learning rate ratio
-)
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', max_keep_ckpts=2)
+)  # CheckpointHook is a default hook that saves checkpoints at specified intervals. To limit the number of saved checkpoints, use the max_keep_ckpts parameter, which deletes older checkpoints once the limit is exceeded
 
-# Runner Configuration
-runner = dict(
-    _delete_=True,  # Delete existing runner settings
-    type='EpochBasedRunner',  # Training based on the number of epochs
-    max_epochs=1  # Maximum number of training epochs
-)
+test_cfg = dict(type='TestLoop')  # Type of testing loop
+
+val_cfg = None  # Type of validation loop
