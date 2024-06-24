810a5fc2184cec6d177418d8babd621c8b2716c7
adithyan sukumar <adithyan.sukumar@clarifai.com>
2024-06-24 13:28:27 +0530

    changed dataset export code snippet

--- code_snippets/python-sdk/managing-datasets/export.py
+++ code_snippets/python-sdk/managing-datasets/export.py
@@ -1,4 +1,7 @@
 from clarifai.client.dataset import Dataset
+import os
+
+os.environ["CLARIFAI_PAT"]="YOUR_PAT"
 
 # The “clarifai-data-protobuf.zip” file can be downloaded from the dataset section in the portal.
-Dataset().export(save_path='output.zip', local_archive_path='clarifai-data-protobuf.zip')
+Dataset().export(save_path='path to output.zip file', local_archive_path='path to clarifai-data-protobuf.zip file')
