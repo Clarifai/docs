44958ea5633eaf2ad52544301469af0c32eba22e
DaniAkash <dani@clarifai.com>
2024-06-28 15:54:23 +0530

    feat: redocly theme

--- src/components/redocly.jsx
+++ src/components/redocly.jsx
@@ -14,7 +14,31 @@ export default function Redocly() {
 
         const initRedoc = () => {
             Redoc.init(window.location.origin + '/api-spec/clarifai.yml', {
-                theme: { colors: { primary: { main: '#0c38f6' } } },
+                theme: {
+                    typography: {
+                        fontSize: getComputedStyle(document.documentElement).getPropertyValue('--ifm-font-size-base').trim(),
+                        fontFamily: getComputedStyle(document.documentElement).getPropertyValue('--ifm-font-family-base').trim(),
+                        headings: {
+                            fontFamily: getComputedStyle(document.documentElement).getPropertyValue('--ifm-font-family-base').trim(),
+                            fontWeight: getComputedStyle(document.documentElement).getPropertyValue('--ifm-heading-font-weight').trim(),
+                            lineHeight: getComputedStyle(document.documentElement).getPropertyValue('--ifm-heading-line-height').trim(),
+                        },
+                    },
+                    sidebar: {
+                        backgroundColor: getComputedStyle(document.documentElement).getPropertyValue('--ifm-background-color').trim(),
+                        textColor: getComputedStyle(document.documentElement).getPropertyValue('--ifm-color-primary-lighter').trim(),
+                        activeTextColor: getComputedStyle(document.documentElement).getPropertyValue('--ifm-color-primary-darker').trim(),
+                    },
+                    rightPanel: {
+                        backgroundColor: getComputedStyle(document.documentElement).getPropertyValue('--ifm-code-background').trim(),
+                        textColor: getComputedStyle(document.documentElement).getPropertyValue('--ifm-color-primary-lighter').trim(),
+                        servers: {
+                            overlay: {
+                                textColor: getComputedStyle(document.documentElement).getPropertyValue('--ifm-color-primary-darker').trim(),
+                            }
+                        }
+                    },
+                },
             }, document.querySelector('#redocly-ui'));
         };
 
@@ -22,6 +46,12 @@ export default function Redocly() {
     }, []);
 
     return (
-        <div id="redocly-ui"/>
+        <>
+            <div id="redocly-ui"/>
+            <style>{`table td:nth-child(2) {
+                    padding-left: 14px !important;
+                }`}
+            </style>
+        </>
     );
 }
