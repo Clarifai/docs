0065925c26d0100ce0ccdbb90617620c3692ac46
Alfrick <alfo.opidi85@gmail.com>
2024-07-16 22:22:15 +0300

    removed legacy search snippets

--- code_snippets/api-guide/search/legacy_search/curl/list_saved_searches.sh
+++
@@ -1,3 +0,0 @@
-curl --location --request GET "https://api.clarifai.com/v2/users/YOUR_USER_ID_HERE/apps/YOUR_APP_ID_HERE/searches" \
---header "Content-Type: application/json" \
---header "Authorization: Key YOUR_PAT_HERE"
