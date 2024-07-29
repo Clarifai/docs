0948aa84308ecd3adbf9da3b9eff193ded9d7a43
Dani Akash <DaniAkash@users.noreply.github.com>
2024-07-29 15:17:53 +0530

    feat: respect user preferred color scheme (#469)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -309,7 +309,7 @@ const config = {
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: false,
-        respectPrefersColorScheme: false,
+        respectPrefersColorScheme: true,
       },
       docs: {
         sidebar: {
