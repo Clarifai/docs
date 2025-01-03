b4a744a2cbc1fb06f8fbc922e5031f9932768bfd
Alfrick Opidi <alfo.opidi85@gmail.com>
2025-01-03 08:32:03 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ const config = {
       announcementBar: {
         id: 'support_us_' + Date.now(),
         content:
-          '<span>New!</span> Clarifai Control Center: Your Unified AI Dashboard <a target="_blank" rel="noopener noreferrer" href="https://www.clarifai.com/blog/clarifai-10.9-control-center">read now</a>',
+          '<span>New!</span> Compute Orchestration [Public-Preview] <a target="_blank" rel="noopener noreferrer" href="https://www.clarifai.com/blog/clarifai-10.10-compute-orchestration-public-preview">read now</a>',
         isCloseable: false,
       },
       navbar: {
