452ea39e76513be746cfdafdc24f910f9fa178b7
Alfrick <alfo.opidi85@gmail.com>
2025-04-11 08:37:23 +0300

    updated applications page

--- code_snippets/python-sdk/create-apps/create_app.py
+++ code_snippets/python-sdk/create-apps/create_app.py
@@ -2,4 +2,4 @@ from clarifai.client.user import User
 
 client = User(user_id="user_id", pat="YOUR_PAT")
 # You can create an app by providing the app id
-app = client.create_app(app_id="test_app", base_workflow="Empty")
+app = client.create_app(app_id="test_app")
