f930fd2086fc2d5f2fbe1cd8bc0c66bd125dee4a
Alfrick <alfo.opidi85@gmail.com>
2025-05-21 15:23:44 +0300

    updated upload files

--- code_snippets/python-sdk/model-upload/upload-first-model.py
+++ code_snippets/python-sdk/model-upload/upload-first-model.py
@@ -1,15 +1,14 @@
 from clarifai.runners.models.model_class import ModelClass
-from clarifai.runners.utils.data_types import Text
 
 
 class MyFirstModel(ModelClass):
     """A custom model that adds 'Hello World' to the end of a text."""
 
     @ModelClass.method
-    def predict(self, text1: Text = "") -> Text:
+    def predict(self, text1: str = "") -> str:
         """
         This is the method that will be called when the model is run.
         It takes in an input and returns an output.
         """
-        output_text = text1.text + " Hello World"
-        return Text(output_text)
+        output_text = text1 + " Hello World"
+        return output_text
