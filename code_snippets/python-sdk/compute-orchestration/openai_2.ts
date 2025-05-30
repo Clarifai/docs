eec9b67e835aa137ff1fc2eacfc642aad4a8d6a0
DaniAkash <dani@clarifai.com>
2025-05-30 19:45:54 +0530

    feat: created examples for js

---
+++ code_snippets/python-sdk/compute-orchestration/openai_2.ts
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+import type { ChatCompletionTool } from "openai/resources";
+
+const client = new OpenAI({
+  baseURL: "https://api.clarifai.com/v2/ext/openai/v1",
+  apiKey: process.env.CLARIFAI_PAT,
+});
+
+const tools: ChatCompletionTool[] = [
+  {
+    type: "function",
+    function: {
+      name: "get_weather",
+      description: "Get current temperature for a given location.",
+      parameters: {
+        type: "object",
+        properties: {
+          location: {
+            type: "string",
+            description: "City and country e.g. Bogotá, Colombia",
+          },
+        },
+        required: ["location"],
+        additionalProperties: false,
+      },
+      strict: true,
+    },
+  },
+];
+
+const toolCompletion = await client.chat.completions.create({
+  model: "https://clarifai.com/openai/chat-completion/models/gpt-4o",
+  messages: [
+    { role: "system", content: "You are a helpful assistant." },
+    { role: "user", content: "What is the weather in New York?" },
+  ],
+  tools,
+});
+
+console.log(toolCompletion.choices?.[0]?.message.tool_calls);
