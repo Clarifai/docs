8ef64598b798cd14129a6a44e69fd181d8f8bc24
Alfrick <macbookpro@macs-MacBook-Pro.local>
2025-09-16 08:30:13 +0300

    updated co docs

--- code_snippets/python-sdk/compute-orchestration/curl_list_3.sh
+++ code_snippets/python-sdk/compute-orchestration/curl_list_3.sh
@@ -1,2 +1,2 @@
-curl -X GET "https://api.clarifai.com/v2/cloud_providers/{{cloud_provider_id}}/regions/{{region_id}}/" \
+curl -X GET "https://api.clarifai.com/v2/cloud_providers/{{cloud_provider_id}}/regions/{{region_id}}/instance_types" \
   -H "Authorization: Key YOUR_PAT_HERE"
