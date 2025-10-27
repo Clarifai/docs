5ed42d217686c1eb44a1185f00a557dfd8b4aacd
Alfrick <macbookpro@macs-MacBook-Pro.local>
2025-10-27 07:54:46 +0300

    updated list inputs

--- code_snippets/python-sdk/managing-inputs/list_input.py
+++ code_snippets/python-sdk/managing-inputs/list_input.py
@@ -1,7 +1,7 @@
 from clarifai.client.user import User
 
 # Create the input object
-input_obj = User(user_id="user_id").app(app_id="test_app", pat="YOUR_PAT").inputs()
+input_obj = User(pat="YOUR_PAT", user_id="user_id").app(app_id="test_app").inputs()
 # list the inputs with pagination
 all_inputs = list(input_obj.list_inputs(page_no=1,per_page=3))
 print(all_inputs)
